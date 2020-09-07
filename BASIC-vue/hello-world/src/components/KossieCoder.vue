7a5f5ea29ce5c82cfd87b97b008ae894dc59a7fb
허진기 <god85a@naver.com>
2020-09-07 18:07:49 +0900

    Vue router , Single File Component, child Component

---
+++ BASIC-vue/hello-world/src/components/KossieCoder.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <p>{{ name }}</p><br>
+        <button @click="updateName">Change Name</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: 'default title',
+        },
+        name: {
+            type: String,
+            default: 'default name',
+        }
+    },
+
+    data() {
+        return{
+        }
+        
+    },
+
+    methods: {
+        updateName() {
+            this.name = 'Kossie Coder Updated';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ BASIC-vue/hello-world/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>This is Home Page</h1>
+      <p>{{ name }}</p>
+      <KossieCoder title="home title"/>
+    </div>
+</template>
+
+<script>
+import KossieCoder from '@/components/KossieCoder.vue';
+export default {
+  components: {
+    KossieCoder
+  },
+  data() {
+    return {
+      name: 'Kossie Coder'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
